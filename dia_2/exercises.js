a1634e5116d60909047f9ae8181a42e5c012b2a0
Gabriel Pereira <g.pereira.a.m@gmail.com>
2021-04-13 20:38:16 -0300

    Exercicio 3

--- dia_2/exercises.js
+++ dia_2/exercises.js
@@ -2,9 +2,16 @@
  const verifiedRandomNumbersPromises = new Promise((resolve, reject) => {
     const numberRandom = Array.from(Array(10), () => Math.floor(Math.random() * 10 + 1));
     const potentiation = numberRandom.map((number) => Math.pow(number, 2));
-    const sumArrays = potentiation.reduce((acc, number) => acc += number);
+    const sumArrays = potentiation.reduce((acc, number) => acc + number);
 
     if(sumArrays < 8000) return resolve(sumArrays);
     return reject(sumArrays);   
 })
+// Exercicio 3
+ .then((sumArrays) => [2, 3, 5, 10].map((number) => sumArrays / number))
+
+
+//  console.log(verifiedRandomNumbersPromises);
+
+
 
