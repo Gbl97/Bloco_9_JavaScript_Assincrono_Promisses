04ba559cb4086df86cd0c65912b60ccf9a395179
Gabriel Pereira <g.pereira.a.m@gmail.com>
2021-04-13 18:32:53 -0300

    Exercicio 1

---
+++ dia_2/exercises.js
@@ -0,0 +1,21 @@
+// Exercicio 1
+const showJoke = ({ joke }) => {
+    const getID = document.getElementById("jokeContainer");
+    getID.innerHTML = joke;
+}
+// apiScript.js
+const API_URL = 'https://icanhazdadjoke.com/';
+
+const fetchJoke = () => {
+  // Adicionar lógica aqui!
+  const myObject = {
+    method: 'GET',
+    headers: { 'Accept': 'application/json' }
+  };
+
+  fetch(API_URL, myObject)
+    .then((response) => response.json())
+    .then((data) => showJoke(data));
+};
+
+window.onload = () => fetchJoke();
