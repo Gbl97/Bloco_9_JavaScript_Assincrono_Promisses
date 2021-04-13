ac8959e9e90b428df8ae09fe1b2a1e8bf6480d6f
Gabriel Pereira <g.pereira.a.m@gmail.com>
2021-04-12 21:27:43 -0300

    Exercicio 6

--- dia_1/exercises.js
+++ dia_1/exercises.js
@@ -77,10 +77,24 @@ const toFahrenheit = (degreeCelsius) => (degreeCelsius * 9/5) + 32;
 const temperatureInFahrenheit = (temperature) => console.log(`It is currently ${toFahrenheit(temperature)}ºF at Mars`);
 const greet = (temperature) => console.log(`Hi there! Curiosity here. Right now is ${temperature}ºC at Mars`);
 
+// const sendMarsTemperature = ((callback) => {
+//   setTimeout(() => {
+//     callback(getMarsTemperature());
+//   }, messageDelay());
+// });
+// sendMarsTemperature(temperatureInFahrenheit); //imprime "It is currently 47ºF at Mars", por exemplo
+// sendMarsTemperature(greet);
+
+// Exercicio 6
+
+const handleError = (errorReason) => console.log(`Error getting temperature: ${errorReason}`);
+
 const sendMarsTemperature = ((callback) => {
   setTimeout(() => {
-    callback(getMarsTemperature());
+    const didOperationSucceed = Math.random() >= 0.4;
+    (didOperationSucceed) ? callback(getMarsTemperature()) : handleError("Robot is busy");
   }, messageDelay());
 });
-sendMarsTemperature(temperatureInFahrenheit); //imprime "It is currently 47ºF at Mars", por exemplo
-sendMarsTemperature(greet);
+
+sendMarsTemperature(temperatureInFahrenheit, handleError);
+sendMarsTemperature(greet, handleError);
